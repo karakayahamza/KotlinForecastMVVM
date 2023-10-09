98b7bfbefc1efb87ad37077fb4248142b0ba2dff
Hamza Karakaya <72933537+karakayahamza@users.noreply.github.com>
2023-10-09 18:51:17 +0300

    Update WeatherModel.kt

--- app/src/main/java/com/example/kotlinweatherforecast/data/model/WeatherModel.kt
+++ app/src/main/java/com/example/kotlinweatherforecast/data/model/WeatherModel.kt
@@ -1,8 +1,5 @@
 package com.example.kotlinweatherforecast.data.model
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 class WeatherModel(uuid: Int, cod: String, message: Int, cnt: Int, weatherList: List<list>, city: City) {
